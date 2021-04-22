09c1d1d8af64a4409a22c36aeaf65dfddb6ba170
RMartins11 <ricardo.martins.1998@hotmail.com>
2021-04-22 12:28:02 +0100

    fixed PORT error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const typeDefs = require("./graphql/typeDefs")
 const resolvers = require("./graphql/resolvers")
 
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 
 
